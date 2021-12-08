7c8a7d8e532b0282bc4e0668c261d013896300f3
Kotolav <kotolav@gmail.com>
2021-12-09 03:02:31 +0400

    fix pivot table visibility

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -9,19 +9,16 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property int $id
  * @property string $tag
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|Tag newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Tag newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Tag query()
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereTag($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Tag extends Model
 {
    public $guarded = ['id'];
+   protected $hidden = ['pivot'];
    public $timestamps = false;
 }
