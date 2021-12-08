e11ff82a94c4bedd65d5502a4a9265e9ab2687e2
Kotolav <kotolav@gmail.com>
2021-12-09 02:59:40 +0400

    add task enum status type

--- app/Enums/TaskStatusType.php
+++ app/Enums/TaskStatusType.php
@@ -7,13 +7,15 @@ use BenSampo\Enum\Enum;
 /**
  * @method static static Added()
  * @method static static Processing()
+ * @method static static Updating()
  * @method static static Done()
  * @method static static Fail()
  */
 final class TaskStatusType extends Enum
 {
-   const Added = 0;
-   const Processing = 1;
-   const Done = 2;
-   const Fail = 3;
+   const Added = 'added';
+   const Processing = 'processing';
+   const Updating = 'updating';
+   const Done = 'done';
+   const Fail = 'fail';
 }
